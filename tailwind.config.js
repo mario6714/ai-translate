64a1cd83ca5cfd2769d6192d6289896804ae33dd
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2024-07-02 00:54:31 -0300

    add boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.tsx"],
+  theme: {
+    extend: {
+        colors: {
+            primary: "#4493F8", 
+            button: "#161B22",
+            placeholder: "#A1A1AA"
+        }
+    },
+  },
+  plugins: [],
+}
+
