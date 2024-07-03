a598b0a9d807468f1f6f41bc7d6923055da8e2da
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2024-07-03 14:54:45 -0300

    styling adjustments

--- src/components/TextBox/index.tsx
+++ src/components/TextBox/index.tsx
@@ -108,15 +108,16 @@ function ConfirmationControls( {store}: {store: [ITextStore, SetStoreFunction<IT
                     save_text(text)
                 }
             } }>
-                <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                <svg class="w-5 h-5 text-primary hover:text-white active:text-white/60" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
                 </svg>
             </button>
+
             <button onclick={ () => { 
                 if (text.editing) { setText('editing', false) }
-                if (text.translated !== textarea?.value && textarea && text.translated) { textarea.textContent = text.translated }
+                if (text.translated !== textarea?.value && textarea && text.translated) { textarea.value = text.translated }
             } }>
-                <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                <svg class="w-5 h-5 text-primary hover:text-white active:text-white/60" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                 </svg>
             </button>
