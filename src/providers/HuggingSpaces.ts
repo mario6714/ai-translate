ae4b1d1ccb6de26323340c58920c3f0c2ad69d11
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2024-07-17 00:48:38 -0300

    Update HuggingSpaces.ts

--- src/providers/HuggingSpaces.ts
+++ src/providers/HuggingSpaces.ts
@@ -53,9 +53,10 @@ class HfSpace extends CustomSSE {
 export async function HfSpaceHandler(text: string, model_name: string, tag: HTMLTextAreaElement): Promise<string> { 
     // https://shuddho-hfllmapi.hf.space/api/v1/chat/completions
     // https://hansimov-hf-llm-api.hf.space/api/v1/chat/completions
+    // https://lintasmediadanawa-hf-llm-api.hf.space/api/v1/chat/completions
     const API_KEY = configs().providers?.HfSpaces?.api_key
     if (!text || !tag || !model_name || !API_KEY) { return "" }
-    const hf = new HfSpace("https://shuddho-hfllmapi.hf.space/api/v1/chat/completions", { 
+    const hf = new HfSpace("https://lintasmediadanawa-hf-llm-api.hf.space/api/v1/chat/completions", { 
         headers: { "Authorization": "Bearer"+" "+API_KEY },
         model: model_name
     })
@@ -95,7 +96,15 @@ export default {
             name: "zephyr-141b",
             owned_by: "Huggingface",
             enabled: undefined
-        }, */ {
+        }, */{ 
+            name: "openchat-3.5",
+            owned_by: "OpenChat",
+            enabled: undefined
+        }, { 
+            name: "Qwen2-72B",
+            owned_by: "Qwen",
+            enabled: undefined
+        }, {
             name: "gpt-3.5-turbo",
             owned_by: "OpenAI",
             enabled: undefined
@@ -116,6 +125,7 @@ export default {
 }
 
 
+// https://huggingface.co/spaces/lintasmediadanawa/hf-llm-api
 // https://huggingface.co/spaces/Hansimov/hf-llm-api
 // https://huggingface.co/spaces/Shuddho/HFLLMAPI
 // https://huggingface.co/spaces/kenken999/fastapi_django_main
