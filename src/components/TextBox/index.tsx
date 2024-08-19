1733a2708609ffdbfd3b43a979866ddfe9fb9181
Rafael Vasconcelos <rafael.vasconcelos7497@gmail.com>
2024-08-19 04:04:39 +0000

    fix: sanitize translated text

--- src/components/TextBox/index.tsx
+++ src/components/TextBox/index.tsx
@@ -55,8 +55,9 @@ export default function TextBox( {modelName: model_name, providerKey, index}: IT
 
     async function translate(options?: {save?: boolean}) { 
         if (handler && textarea && text.untranslated) { 
-            const translated = await handler(text.untranslated, model_name, textarea)
+            const result = await handler(text.untranslated, model_name, textarea)
             .catch(e => e)
+            const translated = result?.replaceAll("\n", "").replaceAll("  ", " ").trim().replaceAll(/^"|"$/g, '')
             setText('translated', translated)
             if (index===0 && options?.save) { save_text(text) }
         }
