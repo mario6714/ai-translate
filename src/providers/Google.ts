05d18be1cc14f569525d9df3b5d1546389e6f1ab
Rafael Vasconcelos <rafael.vasconcelos7497@gmail.com>
2024-08-23 05:12:43 +0000

    fix: update google filters

--- src/providers/Google.ts
+++ src/providers/Google.ts
@@ -6,20 +6,20 @@ import { systemPrompt, userPrompt } from '../global/text'
 const safetySettings = [
     {
       category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-    },
-    {
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    }, {
       category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-    },
-    {
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    }, {
       category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
       threshold: HarmBlockThreshold.BLOCK_NONE,
-    },
-    {
+    }, {
       category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
       threshold: HarmBlockThreshold.BLOCK_NONE,
-    },
+    }, { 
+        category: HarmCategory.HARM_CATEGORY_UNSPECIFIED,
+        threshold: HarmBlockThreshold.BLOCK_NONE
+    }
 ]
 
 
