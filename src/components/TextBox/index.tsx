246ceaf7ac3c18da4ccd0fe652347f9900761f9d
Rafael Vasconcelos <rafael.vasconcelos7497@gmail.com>
2024-10-15 03:59:07 +0000

    update fetched text post-processing

--- src/components/TextBox/index.tsx
+++ src/components/TextBox/index.tsx
@@ -57,7 +57,7 @@ export default function TextBox( {modelName: model_name, providerKey, index}: IT
         if (handler && textarea && text.untranslated) { 
             const result = await handler(text.untranslated, model_name, textarea)
             .catch(e => e)
-            const translated = typeof result !== "string"? result : result?.replaceAll("\n", "").replaceAll("  ", " ").trim()
+            const translated = typeof result !== "string"? result : result?.replaceAll("\n", "").replaceAll("  ", " ").trim().replace(/^"(.*?)"$/, '$1')
             setText('translated', translated)
             if (index===0 && options?.save) { save_text(text) }
         }
