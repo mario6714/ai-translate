2ba63b9c195574247c63dca39ca54cb02e278f84
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2025-03-24 12:11:39 -0300

    Update HuggingFace models

--- src/providers/HuggingFace.ts
+++ src/providers/HuggingFace.ts
@@ -3,13 +3,23 @@ import { configs } from "../global/configs";
 import { systemPrompt, userPrompt } from "../global/text";
 
 
+
+type InferenceProvider = "fireworks-ai" | "together" | "novita" | "nebius" | "hyperbolic" | "sambanova" | "hf-inference"
+
+const inference_models = { 
+    "deepseek-ai/DeepSeek-R1": "fireworks-ai" as InferenceProvider,
+    "deepseek-ai/DeepSeek-V3": "fireworks-ai" as InferenceProvider,
+    "meta-llama/Llama-3.3-70B-Instruct": "fireworks-ai" as InferenceProvider,
+}
+
 export async function HuggingFaceHandler(text: string, model_name: string, tag: HTMLTextAreaElement) { 
     const API_KEY = configs().providers?.HuggingFace?.api_key
     if (!text || !tag || !model_name || !API_KEY) { return "" }
     const inference = new HfInference(API_KEY)
 
     tag.value = ""
-    const stream = inference.chatCompletionStream( {
+    const stream = inference.chatCompletionStream({ 
+        provider: inference_models[model_name as never] ?? "hf-inference",
         model: model_name,
         messages: [ 
             { role: "system", content: systemPrompt },
@@ -17,7 +27,7 @@ export async function HuggingFaceHandler(text: string, model_name: string, tag: 
         ],
         temperature: 0,
         max_tokens: 1024,
-        top_p: 0.3
+        top_p: 0.3, 
     } )
     for await (const chunk of stream) {
         if (chunk.choices && chunk.choices.length > 0) {
@@ -37,14 +47,6 @@ export default {
     api_key: undefined,
     models: [
         /*{
-            name: "llama3-70b",
-            owned_by: "Meta",
-            enabled: undefined
-        }, {
-            name: "command-r-plus",
-            owned_by: "CohereForAI",
-            enabled: undefined
-        }, {
             name: "zephyr-141b",
             owned_by: "Huggingface",
             enabled: undefined
@@ -52,25 +54,41 @@ export default {
             name: "openchat-3.5",
             owned_by: "OpenChat",
             enabled: undefined
-        }, { 
-            name: "Qwen2-72B",
-            owned_by: "Qwen",
-            enabled: undefined
         }, {
             name: "gpt-3.5-turbo",
             owned_by: "OpenAI",
             enabled: undefined
-        },*/ {
-            name: "deepseek-ai/DeepSeek-R1",
-            owned_by: "DeepSeek AI",
-            auto_fetch: true
+        }, {
+            name: "command-r-plus",
+            owned_by: "CohereForAI",
+            enabled: undefined
+        }, {
+            name: "meta-llama/Llama-3.3-70B-Instruct",
+            owned_by: "Meta",
+            enabled: undefined
         }, {
             name: "deepseek-ai/DeepSeek-V3",
             owned_by: "DeepSeek AI",
             auto_fetch: true
         }, {
+            name: "deepseek-ai/DeepSeek-V3-0324",
+            owned_by: "DeepSeek AI",
+            auto_fetch: true
+        }, {
+            name: "deepseek-ai/DeepSeek-R1",
+            owned_by: "DeepSeek AI",
+            auto_fetch: true
+        },*/ {
             name: "Qwen/Qwen2.5-72B-Instruct",
             owned_by: "Qwen",
+            auto_fetch: true
+        }, {
+            name: "Qwen/QwQ-32B",
+            owned_by: "Qwen",
+            auto_fetch: true
+        }, {
+            name: "google/gemma-3-27b-it",
+            owned_by: "Google",
             auto_fetch: true
         }, {
             name: "HuggingFaceH4/starchat2-15b-v0.1",
